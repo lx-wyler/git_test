0d844a9dce1e5115a72e078ccababf95d50a65f1
liuxin <H0932liuxin@mail.sdmc.com>
2024-12-09 10:57:51 +0800

    my first changes

---
+++ include.h
@@ -0,0 +1,12 @@
+#ifndef __INCLUDE_H__
+#define __INCLUDE_H__
+
+
+struct test_t
+{
+    int a;
+    char b[100];
+};
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+
+int main(int argc, char *argv[])
+{
+    int ret = 0;
+
+    printf("It is my git test.");
+
+    return ret;
+}
+
